49a2b32c27ffc51e08419f0580d3cf1fc24b6f24
bencevicbruno <bruno.bencevic@cobeisfresh.com>
2022-08-23 15:13:34 +0200

    Initial Commit

---
+++ Tests/SwiftNetworkingTests/SwiftNetworkingTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import SwiftNetworking
+
+final class SwiftNetworkingTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(SwiftNetworking().text, "Hello, World!")
+    }
+}
